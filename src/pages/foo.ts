b0f4669b6c0d143309f0f3ad9779a6bb8cc7d892
Max Countryman <maxc@me.com>
2022-11-03 13:46:27 -0700

    again

--- src/pages/foo.ts
+++ src/pages/foo.ts
@@ -1,7 +1,11 @@
 import { foo } from "../foo";
 
 export const get = async () => {
+    const x = await foo();
+
     return {
-        body: JSON.stringify({ foo: await foo() }),
+        body: JSON.stringify({
+            foo: x,
+        }),
     };
 };
